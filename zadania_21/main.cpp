20ebdee1a6d3c8fc6e54b98760f383cc2f83f43b
pritexx <bulavinvlad_2004@mail.ru>
2021-09-28 22:25:56 +0300

    Закончил лабу

--- zadania_21/main.cpp
+++ zadania_21/main.cpp
@@ -15,12 +15,12 @@ std::vector<int> getArray(int N, int from = 1, int to = 128){
     return arr;
 }
 
-std::vector<std::vector<int>> getMatrix(int M, int N = 0){
+std::vector<std::vector<int>> getMatrix(int M, int N = 0, int from = 0, int to = 64){
     if(!N) N = M;
     std::vector<std::vector<int>> matrix;
     matrix.reserve(M);
     for(int i{0}; i<M; ++i){
-        matrix.push_back(getArray(N, 0, 64));
+        matrix.push_back(getArray(N, from, to));
     }
     return matrix;
 }
@@ -56,12 +56,116 @@ void task_1(int N){
     arr[N*N-1] = matrix[N/2][N/2];
     cout<<"\n";
     printArray(arr);
+}
 
+int mult(std::vector<int> &arr){
+    int mult{1};
+    for(int i: arr){
+        mult*=i;
+    }
+    return mult;
+}
+
+void task_2(int M, int N, int K){
+    auto matrix{getMatrix(M, N)};
+    printMatrix(matrix);
+
+    int sum{0};
+    for(int i{0}; i<N; ++i){
+        sum+=matrix[K][i];
+    }
+    cout<<"\n";
+    cout<<sum<<" "<<mult(matrix[K]);
+}
+
+void task_3(int M, int N){
+    int index{0};
+    int min{99999999};
+    int cur_mult;
+
+    auto matrix{getMatrix(M, N)};
+    for(int i{0}; i<M; ++i){
+        cur_mult = mult(matrix[i]);
+        if(cur_mult<min){
+            min = cur_mult;
+            index = i;
+        }
+    }
+    cout<<index<<" "<<min;
+}
+
+double getAverage(std::vector<std::vector<int>> &matrix, int column_index){
+    int sum{0};
+    int count{0};
+
+    for(int i{0}; i<matrix.size(); ++i){
+        sum+=matrix[i][column_index];
+        count+=1;
+    }
+    return static_cast<double>(sum)/count;
+}
+
+void task_4(int M, int N){
+    auto matrix{getMatrix(5,5)};
+    printMatrix(matrix);
+    cout<<"\n";
+
+    for(int i{0}; i<N; ++i){
+        double average{getAverage(matrix, i)};
+        int count{0};
+
+        for (int j{0}; j<M; ++j){
+            if (matrix[j][i]>average){
+                ++count;
+            }
+        }
+        cout<<count<<" ";
+    }
+}
+
+int task_5(int M, int N){
+    auto matrix{getMatrix(5,5, 1 , 3)};
+    printMatrix(matrix);
+    cout<<"\n";
+
+    for(int i{0}; i<N; ++i){
+        bool flag = true;
+        for(int j{0}; j<M; ++j){
+            if(matrix[j][i]%2==0){
+                flag = false;
+                break;
+            }
+        }
+        if (flag) return i;
+    }
+    return 0;
 }
 
 int main() {
-    task_1(9);
+    task_1(7);
+    cout<<"\n\n";
+    task_2(5,6,2);
+    cout<<"\n\n";
+    task_3(7,4);
+    cout<<"\n\n";
+    task_4(5,5);
+    cout<<"\n\n";
+    cout<<task_5(5,5);
     cout<<"\n\n";
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
